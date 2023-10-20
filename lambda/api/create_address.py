8042050acdd28493b737ee62fdeca18dffaece39
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2023-10-20 14:58:54 +0200

    Added check address to createAddress

--- lambda/api/create_address.py
+++ lambda/api/create_address.py
@@ -5,9 +5,9 @@ import boto3
 
 from utils.date import get_date_plus_10_min, get_date_now
 from utils.util import generate_new_address
+from utils.check_address import check_active_address
 
-#table_name = os.environ["TABLE_NAME"]
-table_name = "test"
+table_name = os.environ["TABLE_NAME"]
 domain = "mail.castrojonsson.se"
 
 dynamodb = boto3.resource('dynamodb')
@@ -15,6 +15,12 @@ table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
     new_address = generate_new_address()
+
+    if check_active_address(new_address, table):
+        return {
+        'statusCode': 400,
+        'body': json.dumps('address exists')
+    }
 
     new_address_item = {
         'pk': new_address,
