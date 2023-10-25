5fc965bc261e458474b32f60726990c19d6cde34
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2023-10-25 14:06:26 +0200

    Added Delete lambda ands util

---
+++ lambda/api/delete.py
@@ -0,0 +1,36 @@
+import os
+import json
+
+import boto3
+
+from utils.delete import delete_address, delete_mail
+
+table_name = os.environ["TABLE_NAME"]
+bucket_name = os.environ["BUCKET_NAME"]
+
+s3 = boto3.resource("s3")
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(table_name)
+
+
+def lambda_handler(event, context):
+    type = event["queryStringParameters"]["type"]
+    address = event["queryStringParameters"]["address"]
+
+    if type == "address":
+        delete_address(address, table, s3, bucket_name)
+        return {
+            "statusCode": 200,
+            "body": json.dumps(f"successfuly deleted address: {address}"),
+        }
+    elif type == "email":
+        sk = event["queryStringParameters"]["sk"]
+        delete_mail(address, sk, table, s3, bucket_name)
+        return {
+            "statusCode": 200,
+            "body": json.dumps(f"successfuly deleted mail: {sk}, address: {address}"),
+        }
+    else:
+        return {"statusCode": 400, "body": json.dumps("Wrong type")}
+
+    return
---
+++ lambda/api/utils/delete.py
@@ -0,0 +1,50 @@
+from utils.date import get_date_now
+from utils.get_mail import get_emails_ddb
+
+
+def delete_address(address: str, table, s3, bucket_name: str):
+    try:
+        items = get_emails_ddb(address, table)
+
+        table.delete_item(Key={"pk": address, "sk": "address#active"})
+        table.put_item(
+            Item={"pk": address, "sk": "address#inactive", "deleted_at": get_date_now()}
+        )
+
+        with table.batch_writer() as batch:
+            for item in items:
+                batch.delete_item(Key={"pk": item["pk"], "sk": item["sk"]})
+
+        s3_object = [{"key": dictionary["messageId"]} for dictionary in items]
+
+        s3.delete_objects(
+            Bucket=bucket_name,
+            Delete={"Objects": s3_object},
+        )
+
+        print("Successfully deleted")
+        return
+    except Exception as error:
+        print(error)
+        return
+
+
+def delete_mail(address: str, sk: str, table, s3, bucket_name: str):
+    key = {"pk": address, "sk": sk}
+
+    try:
+        response = table.get_item(Key=key)
+        item = response["Item"]
+        message_id = item["messageId"]
+
+        # Takes an S3 clients and deletes the item
+        # TODO Make sure mail obj not delete if used by multiple
+        # TODO Just delete the item and trigger an other function to delete S3 obj
+        s3.delete_object(Bucket=bucket_name, Key="test/" + message_id)
+
+        table.delete_item(Key=key)
+        print("deleted email")
+        return
+    except Exception as error:
+        print(error)
+        return
