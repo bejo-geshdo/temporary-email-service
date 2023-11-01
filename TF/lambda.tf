7bbbb8f9bde668162134f3801fc1dfb632312e0b
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2023-11-01 13:31:37 +0100

    Added Check check_address lambda

--- TF/lambda.tf
+++ TF/lambda.tf
@@ -157,3 +157,31 @@ resource "aws_cloudwatch_log_group" "save_mail" {
   name              = "/aws/lambda/${aws_lambda_function.save_mail.function_name}"
   retention_in_days = 7
 }
+
+data "archive_file" "check_address" {
+  type = "zip"
+  source_dir = "../lambda/api/"
+  output_path = ".terraform/zips/check_address.zip"
+}
+
+resource "aws_lambda_function" "check_address" {
+  filename      = ".terraform/zips/check_address.zip"
+  function_name = "check_address2"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "check_address.lambda_handler"
+
+  source_code_hash = data.archive_file.check_address.output_base64sha256
+
+  runtime = "python3.11"
+
+  environment {
+    variables = {
+      TABLE_NAME  = "email"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "check_address" {
+  name              = "/aws/lambda/${aws_lambda_function.check_address.function_name}"
+  retention_in_days = 7
+}
