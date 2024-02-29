dd2adfd55e2dfcea1aa9b8c2f3084eb090069829
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2024-02-29 09:32:24 +0100

    update to countdown

--- vite-frontend/src/components/CountDown.tsx
+++ vite-frontend/src/components/CountDown.tsx
@@ -4,29 +4,50 @@ interface CountDownProps {
   ttl: number;
 }
 
+interface Time {
+  minutes: number;
+  seconds: number;
+}
+
 const CountDown = ({ ttl }: CountDownProps) => {
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [time, setTime] = useState<Time>({ minutes: 0, seconds: 0 });
 
   useEffect(() => {
-    if (ttl < Math.floor(Date.now() / 1000)) {
-      setMinutes(0);
-      setSeconds(0);
-    } else {
-      const remainingTime = ttl - Math.floor(Date.now() / 1000);
-      const minutes = Math.floor(remainingTime / 60);
-      const seconds = remainingTime % 60;
+    let lastUpdate = Date.now();
+    let animationFrameId: number;
 
-      setMinutes(minutes);
-      setSeconds(seconds);
-    }
+    const tick = () => {
+      const now = Date.now();
+      const delta = now - lastUpdate;
+
+      if (delta >= 1000) {
+        lastUpdate = now;
+
+        if (ttl < Math.floor(now / 1000)) {
+          setTime({ minutes: 0, seconds: 0 });
+        } else {
+          const remainingTime = ttl - Math.floor(now / 1000);
+          const minutes = Math.floor(remainingTime / 60);
+          const seconds = remainingTime % 60;
+
+          setTime({ minutes, seconds });
+        }
+      }
+
+      animationFrameId = requestAnimationFrame(tick);
+    };
+
+    tick(); // start the loop
+
+    return () => cancelAnimationFrame(animationFrameId);
   }, [ttl]);
 
   return (
     <div>
       <h1>Countdown</h1>
       <p>
-        {minutes}:{seconds}
+        {time.minutes < 10 ? `0${time.minutes}` : time.minutes}:
+        {time.seconds < 10 ? `0${time.seconds}` : time.seconds}
       </p>
     </div>
   );
