1898acda24e5747887ebd3bc72e5dc2df1ad8f48
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2024-05-02 23:22:32 +0200

    Added hero with animation

--- vite-frontend/src/App.tsx
+++ vite-frontend/src/App.tsx
@@ -1,11 +1,13 @@
 //import "./App.css";
 import { EmailClient } from "./containers/EmailClient";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <EmailClient />
     </>
   );
--- vite-frontend/src/containers/EmailClient.tsx
+++ vite-frontend/src/containers/EmailClient.tsx
@@ -92,18 +92,6 @@ export const EmailClient = () => {
 
   return (
     <div>
-      <h1>Serverless temporary email experiment</h1>
-
-      <p>This page let's you create a temporary email address</p>
-      <p>The address and emails will be deleted after 10 minutes</p>
-      <p>The address and emails can be renewed by 10 minutes up 24h</p>
-
-      {/* <p>Link to the API docs: </p>
-
-      <a href={window.location.href + "docs"}>
-        {window.location.href + "docs"}
-      </a> */}
-
       {loading ? (
         <p>Loading...</p>
       ) : (
